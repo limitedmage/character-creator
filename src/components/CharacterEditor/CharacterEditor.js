67ac7001879d05fe179e6f8fd59ed5d2884ff32e
Juliana Peña <me@julip.co>
2021-05-05 10:47:10 -0700

    better character placement and sizing

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -26,16 +26,6 @@ function App() {
   return (
     <main className={styles.characterEditor}>
       <MaxWidthWrapper className={styles.maxWidthWrapper}>
-        <div className={styles.characterWrapper}>
-          <Character
-            body={body}
-            head={head}
-            face={face}
-            accessory={accessory}
-            skinColor={skinColor}
-            clothesColor={clothesColor}
-          />
-        </div>
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
           <p className={styles.description}>
@@ -79,6 +69,16 @@ function App() {
             options={clothesColorOptions}
             currentOption={clothesColor}
             handleSelectOption={setClothesColor}
+          />
+        </div>
+        <div className={styles.characterWrapper}>
+          <Character
+            body={body}
+            head={head}
+            face={face}
+            accessory={accessory}
+            skinColor={skinColor}
+            clothesColor={clothesColor}
           />
         </div>
       </MaxWidthWrapper>
