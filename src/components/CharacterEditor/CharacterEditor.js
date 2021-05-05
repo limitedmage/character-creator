5f8ba4073c82873d0485a25369a8f8b3a2a7cc77
Juliana Peña <me@julip.co>
2021-05-05 10:54:26 -0700

    perspective background

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -26,6 +26,7 @@ function App() {
   return (
     <main className={styles.characterEditor}>
       <MaxWidthWrapper className={styles.maxWidthWrapper}>
+        <div className={styles.perspectiveBackground}></div>
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
           <p className={styles.description}>
